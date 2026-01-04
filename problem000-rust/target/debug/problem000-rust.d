c824aebffad040a28f7c7c139d07b6f9539b2fda
imclaren <iain@firesphere.com>
2026-01-04 16:14:22 +1100

    fix: refactor

--- problem000-rust/target/debug/problem000-rust.d
+++
@@ -1,1 +0,0 @@
-/Users/iain/Desktop/euler/problem000-rust/target/debug/problem000-rust: /Users/iain/Desktop/euler/problem000-rust/src/main.rs
---
+++ problem000/problem000-rust/target/debug/problem000-rust.d
@@ -0,0 +1,1 @@
+/Users/iain/src/euler/problem000-rust/target/debug/problem000-rust: /Users/iain/src/euler/problem000-rust/src/main.rs
