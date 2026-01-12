b1e9bb4f1a4dbbb721e4f7236d27ec7a50b17e23
imclaren <iain@firesphere.com>
2026-01-12 21:00:19 +1100

    feat: problem 8

---
+++ problem008/problem008-rust/target/debug/deps/problem008_rust-49d94fd6005c079a.d
@@ -0,0 +1,5 @@
+/Users/iain/src/euler/problem008/problem008-rust/target/debug/deps/problem008_rust-49d94fd6005c079a.d: src/main.rs
+
+/Users/iain/src/euler/problem008/problem008-rust/target/debug/deps/problem008_rust-49d94fd6005c079a: src/main.rs
+
+src/main.rs:
---
+++ problem008/problem008-rust/target/debug/problem008-rust.d
@@ -0,0 +1,1 @@
+/Users/iain/src/euler/problem008/problem008-rust/target/debug/problem008-rust: /Users/iain/src/euler/problem008/problem008-rust/src/main.rs
